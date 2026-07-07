895bc7a2bb7938de55b5e792cd47381e6f7a9787
Suraj S P <161311066+DSATM1@users.noreply.github.com>
2026-07-07 21:30:22 +0530

    Remove duplicate pseudocode and clean up comments

--- Day_2.py
+++ Day_2.py
@@ -1,5 +1,3 @@
-<<<<<<< HEAD
-
 ###----- Pseudocode----##### 
 # rule :
 # create a st
@@ -17,21 +15,4 @@ for char in st:
     if char in 'aeiouAEIOU':
         count += 1
 print(f"Count of Vowels = {count}")"""
-=======
-###----- Pseudocode----##### 
-# rule :
-# create a st
-# init count = 0 
-# for char in st:
-#         if char in 'aeiouAEIOU':
-#               count += 1
-# print(count)
 
-###----Code----###
-"""st = "Hello World"
-count = 0
-for char in st:
-    if char in 'aeiouAEIOU':
-        count += 1
-print(f"Count of Vowels = {count}")"""
->>>>>>> 90e01685c4c06766222e5f9c69ff615916df1966
