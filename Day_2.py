4904547412b2f783aa53fda96f7b64d58b0964aa
Suraj <surajsppokale@gmail.com>
2026-07-07 21:45:10 +0530

    Add pseudocode for negative number summation logic

--- Day_2.py
+++ Day_2.py
@@ -16,3 +16,10 @@ for char in st:
         count += 1
 print(f"Count of Vowels = {count}")"""
 
+###----- Pseudocode----##### 
+# create nums list 
+# total = 0 
+# for each i in nums: 
+#         if i<0: 
+#             total += i 
+# print total
