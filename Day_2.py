8c0c4913b29ee8fb65a419aad3db715ded0bd03a
Suraj <surajsppokale@gmail.com>
2026-07-07 21:28:50 +0530

    Add pseudocode and implementation for counting vowels in a string

---
+++ Day_2.py
@@ -0,0 +1,18 @@
+
+###----- Pseudocode----##### 
+# rule :
+# create a st
+# init count = 0 
+# for char in st:
+#         if char in 'aeiouAEIOU':
+#               count += 1
+# print(count)
+
+
+###----Code----###
+"""st = "Hello World"
+count = 0
+for char in st:
+    if char in 'aeiouAEIOU':
+        count += 1
+print(f"Count of Vowels = {count}")"""
