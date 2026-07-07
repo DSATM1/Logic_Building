90e01685c4c06766222e5f9c69ff615916df1966
Suraj S P <161311066+DSATM1@users.noreply.github.com>
2026-07-07 21:29:14 +0530

    Refactor pseudocode and add vowel counting logic

--- Day_2.py
+++ Day_2.py
@@ -6,3 +6,11 @@
 #         if char in 'aeiouAEIOU':
 #               count += 1
 # print(count)
+
+###----Code----###
+"""st = "Hello World"
+count = 0
+for char in st:
+    if char in 'aeiouAEIOU':
+        count += 1
+print(f"Count of Vowels = {count}")"""
