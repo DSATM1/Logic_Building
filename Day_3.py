595e6db06285ff05fbe8d838aee418b9f440b129
Suraj <surajsppokale@gmail.com>
2026-07-09 21:50:50 +0530

    Add pseudocode for calculating average and count of numbers in a list

---
+++ Day_3.py
@@ -0,0 +1,12 @@
+###----- Pseudocode----##### 
+# create lst
+# find total = sum(lst )← how?
+# find avg = total divided by len(lst ← what goes here?
+# count = 0
+# for each num in lst:
+#              if num > find avg:
+#                  count += 1
+# print avg
+# print count
+
+
