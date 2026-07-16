504b2f01eb6a3709754759a9189518166b7080c4
Suraj <surajsppokale@gmail.com>
2026-07-16 15:51:12 +0530

    Add implementation for finding pairs that sum to target in Day_4.py

--- Day_4.py
+++ Day_4.py
@@ -37,3 +37,11 @@ print(result)"""
 
 
 ###----Code----###
+"""lst = [1, 2, 3, 4, 5]
+target = 5
+result = []
+for i in range(len(lst)):
+    for j in range(i+1, len(lst)):
+        if lst[i] + lst[j] == target:
+            result.append((lst[i],lst[j]))
+print(result)"""  
