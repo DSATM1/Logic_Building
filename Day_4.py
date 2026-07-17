082059adf1a7c033e373de08f298931ff2fc54f7
Suraj <surajsppokale@gmail.com>
2026-07-17 11:13:33 +0530

    Add pseudocode for calculating maximum sum of subarrays in Day_4.py

--- Day_4.py
+++ Day_4.py
@@ -90,3 +90,14 @@ for i in lst:
     result.append(total)
     total += i
 print(result)"""
+
+
+###----- Pseudocode----##### 
+# create lst, k
+# max_sum = 0
+# for i in range(0,len(lst) - k +1):
+#           window = lst[i : i+k]
+#           window_sum = sum(window)
+#           if window_sum > max_sum:
+#                       max_sum = window_sum 
+# print max_sum
