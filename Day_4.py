eb2f1f237828aa41f9a97d84e1d22bd9a5b1549e
Suraj <surajsppokale@gmail.com>
2026-07-23 20:30:48 +0530

    comment

--- Day_4.py
+++ Day_4.py
@@ -116,7 +116,7 @@ print(max_sum)"""
 
 ##--Second largest--##
 
-def sec_larg_num(num):
+"""def sec_larg_num(num):
     largest = 0
     second_largest = 0
 
@@ -127,4 +127,4 @@ def sec_larg_num(num):
         elif n > second_largest:
             second_largest = n
     return second_largest
-print(sec_larg_num([5,13,15,17,6]))
+print(sec_larg_num([5,13,15,17,6]))"""
