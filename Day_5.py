861fcbdfcc1ccc1b7a86378fb0b8a4d8cfcaa006
Suraj <surajsppokale@gmail.com>
2026-07-23 20:33:37 +0530

    Program Added to Day 5 file

---
+++ Day_5.py
@@ -0,0 +1,14 @@
+##--Second largest--##
+
+"""def sec_larg_num(num):
+    largest = 0
+    second_largest = 0
+
+    for n in num:
+        if n > largest:
+            second_largest = largest
+            largest = n 
+        elif n > second_largest:
+            second_largest = n
+    return second_largest
+print(sec_larg_num([5,13,15,17,6]))"""
